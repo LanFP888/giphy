5b3067d47dbc35bef2886089330943cef5328885
LanFP888 <pu.lance@gmail.com>
2018-10-27 01:53:45 -0400

    fav and unfav actions complete

--- asset/js/main.js
+++ asset/js/main.js
@@ -9,6 +9,8 @@ $(document).ready(function () {
   //loads the favlist from local storage, make sure this is first step because there are other functions that depends on the data of this list
   getFav()
 
+  addFavGiftoDiv()
+
   // Calling the renderButtons function to display the intial buttons
   renderButtons(gifList);
 
@@ -33,11 +35,11 @@ $(document).ready(function () {
 
   //stores the fav Gifs
   $(document).on("click", ".saveFav", toggleFav);
-
   // Generic function for displaying the movieInfo
   $(document).on("click", ".gifTopic", displayGifInfo);
-
+  //click on the gif to play the gif
   $(document).on("click", ".gifResult", playGif);
+  $(document).on("click", ".removeFavBtn", rmFav);
 });
 
 // Function for dumping the JSON content for each button into the div
@@ -83,23 +85,65 @@ function getGif(gifTopic) {
   });
 }
 
+function addFavGiftoDiv() {
+  //iterate through the id in the fav list
+  for (var i = 0; i < gifFavList.length; i++) {
+    //call the gitGifbyID function to call the Giphy API with the id
+    getGifbyID(gifFavList[i])
+  }
+}
+
 function getGifbyID(gifID) {
-  queryURL = "http://api.giphy.com/v1/gifs/" + gifID + "?api_key=YOUR_API_KEY"
+  //if we search using the ID, the response returns an object
+  queryURL = "http://api.giphy.com/v1/gifs/" + gifID + "?api_key=kGOO42dDHjBstMYP3bEXkTG3g9nUihvo"
   //ajax call using the queryURL
   $.ajax({
     method: "GET",
     url: queryURL
   }).then(function (response) {
     console.log(response);
-    makeGifHTML(response.data);
+    makeFavGifHTML(response);
   });
+}
+
+function makeFavGifHTML(responseObj) {
+  var gifDiv = $("<div>");
+  gifDiv.addClass("col-sm-6 col-md-4 col-xl-2 favGifObj");
+  gifDiv.attr("data-gif-id", responseObj.data.id)
+  var gifRating = $("<h4>");
+  //save the id of the gif for the removeFavFromDiv function
+  gifRating.html("Rated: " + responseObj.data.rating);
+  var gifPreview = $("<img>");
+  gifPreview.addClass("gifResult");
+  //store the still and gif in two data properties, toggle between them when clicking on the gif
+  gifPreview.attr(
+    "data-gif-still",
+    responseObj.data.images.fixed_width_still.url
+  );
+  gifPreview.attr("data-gif", responseObj.data.images.fixed_width.url);
+  gifPreview.attr("src", responseObj.data.images.fixed_width_still.url);
+  var downloadBtn = $("<a>")
+  // downloadBtn.attr("type", "button")
+  downloadBtn.addClass("btn btn-info col-12 downloadGif")
+  downloadBtn.attr("href", responseObj.data.images.fixed_width.url)
+  downloadBtn.attr("download", "myGif")
+  downloadBtn.attr("target", "_blank")
+  downloadBtn.html("Download")
+  var downloadIcon = $("<i>")
+  downloadIcon.addClass("fa fa-download")
+  var removeBtn = $("<button>")
+  removeBtn.addClass("btn btn-danger col-12 removeFavBtn")
+  removeBtn.html("Remove")
+  var hr = $("<hr>")
+  $(".favGif").prepend(gifDiv.append(gifRating, gifPreview, downloadBtn.prepend(downloadIcon), removeBtn, hr));
+
 }
 
 //function to render the gifs on the screen using the API response
 function makeGifHTML(responseArr) {
   for (var i = 0; i < responseArr.length; i++) {
     var gifDiv = $("<div>");
-    gifDiv.addClass("col-sm-6 col-md-4 col-xl-2");
+    gifDiv.addClass("col-sm-6 col-md-4 col-xl-2 gifObj");
     var gifRating = $("<h4>");
     gifRating.html("Rated: " + responseArr[i].rating);
     var gifPreview = $("<img>");
@@ -168,6 +212,30 @@ function refreshFavStatus() {
   })
 }
 
+function rmFav() {
+  var gifID = $(this).parent(".favGifObj").attr("data-gif-id")
+  // console.log(gifID)
+  //removes the whole div of the gif
+  $(this).parent(".favGifObj").remove()
+  //update the fav list
+  //update the localstorage
+  removeFav(gifID)
+  //refresh the star status on the same gifs on the page
+  refreshFavStatus()
+}
+
+//this method is used for when user clicks on the star on the list of fav page, there's a remove button under the gifs in the fav section
+function removeFavFromDiv(favID) {
+  //$( "p" ).parent( ".selected" ).css( "background", "yellow" );
+  $('.favGifObj').each(function (index, element) {
+    var gifID = $(element).attr("data-gif-id")
+    if (gifID === favID) {
+      //if the id matches
+      $(element).remove()
+    }
+  })
+}
+
 function toggleFav() {
   var gifID = $(this).attr("data-gif-id")
   //store the value if the fav List doesn't already contain the id
@@ -176,11 +244,15 @@ function toggleFav() {
     $(this).css("color", "yellowgreen")
     //store the id in local storage
     storeFav(gifID)
+    //add the fav to the fav Div
+    getGifbyID(gifID)
   } else {
     //remove color from star
     $(this).css("color", "")
     //remove the id in local storage
     removeFav(gifID)
+    //remove the gif from the fav section
+    removeFavFromDiv(gifID)
   }
   refreshFavStatus()
 }
